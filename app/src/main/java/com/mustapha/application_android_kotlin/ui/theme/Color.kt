9d1c3b1ea19d42a8d340fcd95e4e28b5abebfe3b
MustaphaBousilDev <bousilmustapha@gmail.com>
2025-07-12 15:03:01 +0100

    add spacing config

--- app/src/main/java/com/mustapha/application_android_kotlin/ui/theme/Color.kt
+++ app/src/main/java/com/mustapha/application_android_kotlin/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package com.mustapha.application_android_kotlin.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -10,7 +11,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-object EcommerceColors {
+object MugiColors {
     val Primary500 = Color(0xFFFF6B35)      // Orange
     val Primary400 = Color(0xFFFF8E00)      // Orange-Yellow
     val Primary300 = Color(0xFFFFD700)      // Gold
@@ -25,4 +26,13 @@ object EcommerceColors {
     val Error = Color(0xFFEF4444)
     val Warning = Color(0xFFF59E0B)
 
+}
+
+object MugiSpacing {
+    val xs = 4.dp
+    val sm = 8.dp
+    val md = 16.dp
+    val lg = 24.dp
+    val xl = 32.dp
+    val xxl = 48.dp
 }
