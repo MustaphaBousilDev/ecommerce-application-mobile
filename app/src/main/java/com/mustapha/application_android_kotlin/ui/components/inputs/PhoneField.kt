ef6dac86b03e1ae0a6d58500f8967138483aedc0
MustaphaBousilDev <bousilmustapha@gmail.com>
2025-07-12 15:01:36 +0100

    create subcomponent for LoginScreen with constant colors

--- app/src/main/java/com/mustapha/application_android_kotlin/ui/components/inputs/PhoneField.kt
+++ app/src/main/java/com/mustapha/application_android_kotlin/ui/components/inputs/PhoneField.kt
@@ -1,4 +1,2 @@
 package com.mustapha.application_android_kotlin.ui.components.inputs
 
-class SearchField {
-}
--- app/src/main/java/com/mustapha/application_android_kotlin/ui/theme/Color.kt
+++ app/src/main/java/com/mustapha/application_android_kotlin/ui/theme/Color.kt
@@ -9,3 +9,20 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+object EcommerceColors {
+    val Primary500 = Color(0xFFFF6B35)      // Orange
+    val Primary400 = Color(0xFFFF8E00)      // Orange-Yellow
+    val Primary300 = Color(0xFFFFD700)      // Gold
+
+    // Neutral Colors
+    val Neutral900 = Color(0xFF1F2937)      // Dark Gray
+    val Neutral500 = Color(0xFF6B7280)      // Medium Gray
+    val Neutral100 = Color(0xFFF9FAFB)      // Light Gray
+
+    // Semantic Colors
+    val Success = Color(0xFF10B981)
+    val Error = Color(0xFFEF4444)
+    val Warning = Color(0xFFF59E0B)
+
+}
